c6f9db7ae73af7c77ccdc8bbb38837517b4aa8f4
takechi-scratch <takechi.scratch@gmail.com>
2025-09-15 22:47:16 +0900

    add: search(in progress)

--- src/app/songs/page.tsx
+++ src/app/songs/page.tsx
@@ -3,7 +3,36 @@
 import MyAppShell from "@/components/appshell";
 import CardsList from "@/components/songCards/cardsList";
 import { useSongs } from "@/hooks/songs";
-import { Title } from "@mantine/core";
+import { FilterableKeys, SortableKeys } from "@/lib/search/filter";
+import { Title, Tabs, Accordion, Select, TextInput, Button } from "@mantine/core";
+
+function SearchBar() {
+    return (
+        <>
+            <Tabs defaultValue="filter">
+                <Tabs.List grow justify="center" mb="md">
+                    <Tabs.Tab value="filter">絞り込み</Tabs.Tab>
+                    <Tabs.Tab value="nearest">似ている曲</Tabs.Tab>
+                </Tabs.List>
+                <Tabs.Panel value="filter">
+                    {Object.keys(FilterableKeys).map((key) => (
+                        <TextInput key={key} label={key} placeholder={`Enter ${key}`} />
+                    ))}
+                    <Select
+                        data={Object.keys(SortableKeys)}
+                        label="並び替え"
+                        placeholder="1つ選択"
+                        mt="md"
+                    />
+                </Tabs.Panel>
+                <Tabs.Panel value="nearest">現在準備中！</Tabs.Panel>
+            </Tabs>
+            <Button fullWidth mt="md">
+                検索
+            </Button>
+        </>
+    );
+}
 
 export default function Page() {
     const { songs, loading, error } = useSongs();
@@ -16,6 +45,16 @@ export default function Page() {
             <Title order={2} mb="md">
                 曲一覧
             </Title>
+
+            <Accordion variant="separated" m="md">
+                <Accordion.Item key="検索" value="検索">
+                    <Accordion.Control icon="🔍">検索</Accordion.Control>
+                    <Accordion.Panel>
+                        <SearchBar />
+                    </Accordion.Panel>
+                </Accordion.Item>
+            </Accordion>
+
             <CardsList songs={songs} />
         </MyAppShell>
     );
---
+++ src/lib/search/filter.ts
@@ -0,0 +1,23 @@
+export const FilterableKeys = {
+    動画のID: "id",
+    タイトル: "title",
+    ボーカル: "vocal",
+    イラスト等: "illustrations",
+    動画: "movie",
+    コメント: "comment",
+    主なコード進行: "mainChord",
+    主なキー: "mainKey",
+};
+
+export const SortableKeys = {
+    動画のID: "id",
+    タイトル: "title",
+    公開日時: "publishedTimestamp",
+    曲の長さ: "durationSeconds",
+    BPM: "bpm",
+    キー: "mainKey",
+    "6451進行の割合": "chordRate6451",
+    "4561進行の割合": "chordRate4561",
+    ピアノの使用度: "pianoRate",
+    転調回数: "modulationTimes",
+};
