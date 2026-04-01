9b286f856d412ed6852f4425acd8a571fe0553ce
takechi-scratch <takechi.scratch@gmail.com>
2026-04-01 22:18:04 +0900

    update: end april fool

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,7 +80,6 @@ export async function RootLayout({ children }: { children: React.ReactNode }) {
                         </AuthProvider>
                     </ModalsProvider>
                 </MantineProvider>
-                ;
             </body>
         </html>
     );
--- src/components/appshell/myAppshell.tsx
+++ src/components/appshell/myAppshell.tsx
@@ -167,7 +167,6 @@ export default function MyAppShell({
                                     width={40}
                                     height={40}
                                     style={{ marginRight: 8 }}
-                                    className="spin"
                                 />
                                 <Text size="lg" fw={700} style={{ margin: 0 }}>
                                     MIMIさん全曲紹介
