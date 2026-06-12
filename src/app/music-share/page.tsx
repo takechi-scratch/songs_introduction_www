bc1b29a25ed8b67cb29ac15da4a34d74830e42ef
takechi-scratch <takechi.scratch@gmail.com>
2026-06-12 22:37:40 +0900

    add: ogp

--- src/app/music-share/page.tsx
+++ src/app/music-share/page.tsx
@@ -1,5 +1,37 @@
 import { fetchAllSongs } from "@/lib/songs/api";
+import { Metadata } from "next";
 import Playlist from "./playlist";
+
+const title = "音楽シェア | MIMIさん全曲紹介";
+const description = "MIMIさんの曲をリアルタイムで一緒に聴こう！";
+const imageUrl = "https://mimi.takechi.f5.si/assets/card.png";
+
+export const metadata: Metadata = {
+    title: title,
+    description: description,
+    openGraph: {
+        title: title,
+        description: description,
+        url: imageUrl,
+        siteName: title,
+        images: [
+            {
+                url: imageUrl,
+                width: 1200,
+                height: 630,
+                alt: title,
+            },
+        ],
+        locale: "ja_JP",
+        type: "website",
+    },
+    twitter: {
+        card: "summary",
+        title: title,
+        description: description,
+        images: [imageUrl],
+    },
+};
 
 export default async function Page() {
     const songs = await fetchAllSongs();
